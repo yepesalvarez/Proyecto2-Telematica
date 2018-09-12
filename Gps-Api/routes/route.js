ecbb59f94254abefe2306a63b63060046a50f0b6
stiag0 <stiago01r@gmail.com>
2018-09-12 09:46:52 -0500

    este commit es solo para hacer las pruebas con jmeter,no tiene la restriccion de autenticacion

--- Gps-Api/routes/route.js
+++ Gps-Api/routes/route.js
@@ -3,11 +3,11 @@
 var express = require ('express');
 var routeController = require('../controllers/route');
 var api = express.Router();
-var middlewareAuth = require('../middlewares/authenticated')
+//var middlewareAuth = require('../middlewares/authenticated')
 
-api.get('/route/:id', middlewareAuth.ensureAuth, routeController.getRouteById);
-api.post('/route', middlewareAuth.ensureAuth, routeController.saveRoute);
-api.put('/route/:id', middlewareAuth.ensureAuth, routeController.updateRoute);
-api.get('/route/user/:userId', middlewareAuth.ensureAuth, routeController.getRoutesByUser);
+api.get('/route/:id',  routeController.getRouteById);
+api.post('/route',  routeController.saveRoute);
+api.put('/route/:id',  routeController.updateRoute);
+api.get('/route/user/:userId',  routeController.getRoutesByUser);
 
 module.exports = api;
--- Gps-Api/routes/user.js
+++ Gps-Api/routes/user.js
@@ -5,8 +5,8 @@ var userController = require('../controllers/user');
 var api = express.Router();
 var middlewareAuth = require('../middlewares/authenticated')
 
-api.get('/user', middlewareAuth.ensureAuth, userController.getUsers);
-api.get('/user/:id', middlewareAuth.ensureAuth, userController.getUserById);
+api.get('/user', userController.getUsers);
+api.get('/user/:id', userController.getUserById);
 api.post('/register', userController.saveUser);
 api.post('/login', userController.loginUser);
 
